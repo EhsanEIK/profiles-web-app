145ec88bba7cd5aa48a78841386a0d035d840718
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-12-20 12:23:47 +0600

    added TopHeader in Home page

---
+++ src/Components/TopHeader/TopHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import banner from '../../assets/images/cover.png';
+
+const TopHeader = () => {
+    return (
+        <section>
+            <img src={banner} alt="" />
+        </section>
+    );
+};
+
+export default TopHeader;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../../Components/Navbar/Navbar';
+import TopHeader from '../../Components/TopHeader/TopHeader';
 
 const Home = () => {
     return (
         <div className='container mx-auto'>
             <Navbar></Navbar>
+            <TopHeader></TopHeader>
         </div>
     );
 };
