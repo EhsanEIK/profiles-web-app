34be52157f1e7a0354d3b021b02b38a80264588a
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-12-20 14:15:23 +0600

    added CardTwo and CardThree in Cards

--- src/Components/Cards/CardTwo.js
+++ src/Components/Cards/CardTwo.js
@@ -2,7 +2,7 @@ import React from 'react';
 import dentist from '../../assets/images/dentist.png';
 import bell from '../../assets/images/bell.png';
 
-const CardOne = () => {
+const CardTwo = () => {
     return (
         <div className="card card-compact w-96 bg-base-100 shadow-xl">
             <figure><img src={dentist} alt="Dentist" /></figure>
@@ -18,8 +18,11 @@ const CardOne = () => {
                         <p className='text-[#878787]'>11:00 a.m.-6:00 p.m.</p>
                     </div>
                     <div className='flex items-center gap-x-2'>
-                        <img src={bell} alt="bell" className='w-5' />
-                        <p className='text-lg text-[#FF9900] font-medium'>Pending</p>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FF0000" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                        </svg>
+                        <p className='text-lg text-[#FF0000] font-medium'>248</p>
                     </div>
                 </div>
             </div>
@@ -27,4 +30,4 @@ const CardOne = () => {
     );
 };
 
-export default CardOne;
+export default CardTwo;
--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import CardOne from './CardOne';
+import CardThree from './CardThree';
+import CardTwo from './CardTwo';
 
 const Cards = () => {
     return (
-        <div>
+        <div className='grid grid-cols-3 gap-8'>
             <CardOne></CardOne>
+            <CardTwo></CardTwo>
+            <CardThree></CardThree>
         </div>
     );
 };
